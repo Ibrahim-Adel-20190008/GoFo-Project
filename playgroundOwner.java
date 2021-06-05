69cf4c93220b40a835428e1b3d61df0eba89309c
AhmedeZ2001 <81775740+AhmedeZ2001@users.noreply.github.com>
2021-06-05 23:34:43 +0200

    Update playgroundOwner.java

--- playgroundOwner.java
+++ playgroundOwner.java
@@ -1,14 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package swe4;
 
-/**
- *
- * @author Ahmed Abdelnaser
- */
 public class playgroundOwner implements User {
     private String name,email,address, phone;
 
