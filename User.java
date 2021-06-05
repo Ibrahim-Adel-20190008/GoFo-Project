abfb168d4844bcaeeb0f4b7667d8e0f23598f6bc
Ibrahim-Adel-20190008 <84779649+Ibrahim-Adel-20190008@users.noreply.github.com>
2021-06-05 21:27:13 +0000

    Create User.java
    Interface Class User

---
+++ User.java
@@ -0,0 +1,54 @@
+/**
+ *
+ * @author Ibrahim_Adel
+ */
+public interface User {
+
+    /**
+     *
+     * @param name
+     */
+    public void setName(String name);
+
+    /**
+     *
+     * @return
+     */
+    public String getName();
+
+    /**
+     *
+     * @param address
+     */
+    public void setAddress(String address);
+
+    /**
+     *
+     * @return
+     */
+    public String getAddress();
+
+    /**
+     *
+     * @param email
+     */
+    public void setEmail(String email);
+
+    /**
+     *
+     * @return
+     */
+    public String getEmail();
+
+    /**
+     *
+     * @param phone
+     */
+    public void setPhone(String phone);
+
+    /**
+     *
+     * @return
+     */
+    public String getPhone();
+}
