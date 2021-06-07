e2e19a51d282ca4b306a2f6474fac963cff93c78
Ibrahim-Adel-20190008 <84779649+Ibrahim-Adel-20190008@users.noreply.github.com>
2021-06-07 23:45:57 +0000

    Update playgroundOwner.java
    add function Booking into class

--- playgroundOwner.java
+++ playgroundOwner.java
@@ -1,9 +1,34 @@
-
+import java.util.ArrayList;
 public class playgroundOwner implements User {
-    private String name,email,address, phone;
 
     /**
-     *
+     * to store name of playgroundOwner
+     */
+    private String name;
+
+    /**
+     * to store email of playgroundOwner
+     */
+    private String email;
+
+    /**
+     * to store address of playgroundOwner
+     */
+    private String address; 
+    
+    /**
+     * to store address of playgroundOwner
+     */
+    private String phone;
+
+    /**
+     * to store all playgrounds of playgroundOwner
+     */
+    public ArrayList<Playground> PlayGrounds;
+    
+
+    /**
+     * parametric constructor
      * @param name pass the owner name
      * @param email pass the owner email
      * @param address pass the owner address
@@ -14,7 +39,8 @@ public class playgroundOwner implements User {
             this.email=email;
             this.phone=phone;
             this.address=address;
- 
+            PlayGrounds = new ArrayList<>();
+            
         }
 
     /**
@@ -56,7 +82,7 @@ public class playgroundOwner implements User {
 
     /**
      *
-     * @return name
+     * @return name of playgroundOwner
      */
         @Override
     public String getName(){
@@ -65,7 +91,7 @@ public class playgroundOwner implements User {
 
     /**
      *
-     * @return email
+     * @return email of playgroundOwner
      */
         @Override
     public String getEmail(){
@@ -74,7 +100,7 @@ public class playgroundOwner implements User {
 
     /**
      *
-     * @return address
+     * @return address of playgroundOwner
      */
         @Override
     public String getAddress(){
@@ -83,7 +109,7 @@ public class playgroundOwner implements User {
 
     /**
      *
-     * @return phone
+     * @return phone of playgroundOwner
      */
 
 
@@ -91,16 +117,78 @@ public class playgroundOwner implements User {
     public String getPhone(){
             return phone;
         }
-
-    @Override
-    public String toString() {
-        return "PlayGroundOwner{" + "name=" + name + ", email=" + email + ", address=" + address + ", phone=" + phone + '}';
+    
+    /**
+     * to add new Playground to his Owner
+     * @param Playground1
+     * 
+     */
+    
+    public void addPlayground(Playground Playground1){
+        PlayGrounds.add(Playground1);
     }
 
     /**
-     *
-     * @return
+     * This function book the playground 
+     * @param startTime
+     * @param numOfhours
+     * @param indexOfplayground
      */
+    public void Booking(int startTime,int numOfhours,int indexOfplayground)
+    {
+        int numOfhours1=numOfhours;
+        int f;
+        for(int i=0;i<12;i++)
+        {
+            if(PlayGrounds.get(indexOfplayground).hours[i]==startTime ||PlayGrounds.get(indexOfplayground).hours[i]==startTime*-1)
+            {
+                f=i;
+                while(numOfhours>1)
+                {
+                    if(PlayGrounds.get(indexOfplayground).hours[i+1]>0)
+                    {
+                    i++;
+                    numOfhours--;
+                    }
+                    else {
+                        System.out.println("your Booking is rejected");
+                        return;
+                    }
+                }
+                numOfhours=numOfhours1;
+                i=f;
+                if(PlayGrounds.get(indexOfplayground).hours[i]<0) PlayGrounds.get(indexOfplayground).hours[i]=0;
+                else PlayGrounds.get(indexOfplayground).hours[i]=PlayGrounds.get(indexOfplayground).hours[i]*-1;
+                if(PlayGrounds.get(indexOfplayground).hours[i+numOfhours]<0) PlayGrounds.get(indexOfplayground).hours[i+numOfhours]=0;
+                else PlayGrounds.get(indexOfplayground).hours[i+numOfhours]=PlayGrounds.get(indexOfplayground).hours[i+numOfhours]*-1;
+                while(numOfhours>1)
+                {
+                    PlayGrounds.get(indexOfplayground).hours[i+1]=0;
+                    i++;
+                    numOfhours--;
+                }
+                break;
+            }
+        } 
+        System.out.println("your Booking is accepted");
+    }
+ /**
+     *
+     * @return all values of playgroundOwner
+     */
+    @Override
+    public String toString() {
+        return "playgroundOwner{" + "name=" + name + ", email=" + email + ", address=" + address + ", phone=" + phone + '}';
+    }
     
-        
+    /**
+     *
+     * @print all values of playgroundOwner and his Playgrounds
+     */
+    public void Display()
+    {
+       System.out.print(toString());
+        for(int i=0 ;i<PlayGrounds.size() ;i++)
+           System.out.println(PlayGrounds.get(i).display()); 
+    }    
 }
