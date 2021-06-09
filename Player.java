cd280f784328fe49fd0b3b02cbea7845cf32e5ef
Ibrahim-Adel-20190008 <84779649+Ibrahim-Adel-20190008@users.noreply.github.com>
2021-06-09 23:34:32 +0000

    Update Player.java
    add function viewingPlayground

--- Player.java
+++ Player.java
@@ -1,3 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ll;
+
+/**
+ *
+ * @author abdallah mohamed
+ */
 public class Player implements User{
     
     /**
@@ -146,5 +157,13 @@ public class Player implements User{
                 +"\nphone:"+getPhone();
                     return info;
 	}
-        
+
+    /**
+     * To display all Playgrounds for player to choose which Playground he want book him
+     * @param Playgrounds
+     */
+    public void viewingPlayground(registeredPlayground Playgrounds)
+    {
+        Playgrounds.viewingPlayground();
+    }
 }
