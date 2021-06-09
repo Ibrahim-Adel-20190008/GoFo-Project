854dc9a0477fb3b75ddfd7f5e42f0a428e575eef
Ibrahim-Adel-20190008 <84779649+Ibrahim-Adel-20190008@users.noreply.github.com>
2021-06-09 23:35:56 +0000

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,14 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package ll;
 
-/**
- *
- * @author abdallah mohamed
- */
 public class Player implements User{
     
     /**
